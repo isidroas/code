c984adde865b30e7ebcac4e3162893b90f2d2e2e
Isidro Arias <isidroariass@hotmail.es>
2022-04-09 11:57:07 +0200

    working tests!
    It's appear that duplication is not a problem

--- repository.py
+++ repository.py
@@ -17,9 +17,35 @@ class SqlRepository(AbstractRepository):
         self.session = session
 
     def add(self, batch):
-        # self.session.execute('INSERT INTO ??
-        ...
+        eta = 'NULL' if batch.eta is None else batch.eta
+        self.session.execute('INSERT INTO batches (reference, sku, _purchased_quantity, eta)'
+                             f' VALUES ("{batch.reference}", "{batch.sku}", {batch._purchased_quantity}, {eta})')
+
+        [batch_id] = next(self.session.execute(
+            f'SELECT id FROM batches WHERE reference="{batch.reference}"',
+        ))
+
+        for orderline in batch._allocations:
+            self.session.execute('INSERT INTO order_lines (sku, qty, orderid) '
+                                 f'VALUES ("{orderline.sku}", {orderline.qty}, "{orderline.orderid}")')
+
+            [orderline_id] = next(self.session.execute(
+                f'SELECT id FROM order_lines WHERE orderid="{orderline.orderid}"',
+            ))
+
+            self.session.execute('INSERT INTO allocations (orderline_id, batch_id)'
+                                 f'VALUES ({orderline_id}, {batch_id})')
 
     def get(self, reference) -> model.Batch:
-        # self.session.execute('SELECT ??
-        ...
+        id_, reference, sku, _purchased_quantity, eta = next(self.session.execute(f'SELECT * FROM batches WHERE reference="{reference}"' ))
+        batch =model.Batch(reference, sku, _purchased_quantity, eta) 
+
+
+        res = self.session.execute(f'SELECT orderline_id FROM allocations WHERE batch_id={id_}' )
+        for [orderline_id] in res:
+            id2, sku2, qty, orderid  = next(self.session.execute(f'SELECT * FROM order_lines WHERE id={orderline_id}' ))
+            orderline = model.OrderLine(orderid, sku, qty)
+            batch._allocations.add(orderline)
+            
+
+        return batch
