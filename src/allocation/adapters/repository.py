75512e48d8d68e433642208ff39b2c785c66cccc
Isidro Arias <isidroariass@hotmail.es>
2022-06-12 14:32:26 +0200

    copied sqlrepo from solutions

--- src/allocation/adapters/repository.py
+++ src/allocation/adapters/repository.py
@@ -15,19 +15,8 @@ class SqlAlchemyRepository(AbstractRepository):
     def __init__(self, session):
         self.session = session
 
-    #def add(self, batch):
-    #    self.session.add(batch)
-    def add(self, product: model.Product):
-        batches = product.batches
-        for b in batches:
-            res = self.session.query(model.Batch).filter_by(ref=ref).all()
-            if not res:
-                self.session.add(b)
-             
+    def add(self, product):
+        self.session.add(product)
 
     def get(self, sku):
-        batches = self.session.query(model.Batch).filter_by(sku=sku).all()
-        return model.Product(sku, batches)
-
-#    def list(self):
-#        return self.session.query(model.Batch).all()
+        return self.session.query(model.Product).filter_by(sku=sku).first()
