b6a078b56c53a2dc4db963659c329034288104d3
Isidro Arias <isidroariass@hotmail.es>
2022-06-12 13:09:46 +0200

    working integration test

--- src/allocation/adapters/repository.py
+++ src/allocation/adapters/repository.py
@@ -3,11 +3,11 @@ from allocation.domain import model
 
 
 class AbstractRepository(abc.ABC):
-    def add(self, batch: model.Batch):
+    def add(self, product: model.Product):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get(self, reference) -> model.Batch:
+    def get(self, sku) -> model.Product:
         raise NotImplementedError
 
 
@@ -15,11 +15,19 @@ class SqlAlchemyRepository(AbstractRepository):
     def __init__(self, session):
         self.session = session
 
-    def add(self, batch):
-        self.session.add(batch)
+    #def add(self, batch):
+    #    self.session.add(batch)
+    def add(self, product: model.Product):
+        batches = product.batches
+        for b in batches:
+            res = self.session.query(model.Batch).filter_by(ref=ref).all()
+            if not res:
+                self.session.add(b)
+             
 
-    def get(self, reference):
-        return self.session.query(model.Batch).filter_by(reference=reference).one()
+    def get(self, sku):
+        batches = self.session.query(model.Batch).filter_by(sku=sku).all()
+        return model.Product(sku, batches)
 
-    def list(self):
-        return self.session.query(model.Batch).all()
+#    def list(self):
+#        return self.session.query(model.Batch).all()
